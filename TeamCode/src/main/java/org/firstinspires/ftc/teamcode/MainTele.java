234845b401917a0e452164912155e11bfb09e13e
Alex <alexrhabacker@gmail.com>
2023-11-01 11:19:03 -0400

    Flipped the setPosition and updated verb.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
@@ -106,10 +106,11 @@ public class MainTele extends RobotCore {
             }
 
 
-            if(toggleBucketDoorInput){; // no random semi-colons ~ NW
+            if(toggleBucketDoorInput){
                 if(bucketDoorClosed) {
+                    bucketDoor.setPosition(0.925);
                     bucketDoorClosed = false;
-                    bucketDoor.setPosition(0.925); // Maybe try flipping these? like set the position first then change the variable value ~ NW
+                     // Maybe try flipping these? like set the position first then change the variable value ~ NW
                 } else if (!bucketDoorClosed){
                     bucketDoorClosed = true;
                     bucketDoor.setPosition(0.75);
