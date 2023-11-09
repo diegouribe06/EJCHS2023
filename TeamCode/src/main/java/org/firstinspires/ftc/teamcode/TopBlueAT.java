6d2019779ccc66bb56b2946ebbe5517caf660d11
DeEric Burns <dburns26@jcss.us>
2023-11-09 10:07:51 -0500

    Added TopBlueAT java file for first autonomous tests.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAT.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "TopBlue", group = "17421 Autonomous")
+public class TopBlueAT extends OpMode {
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
