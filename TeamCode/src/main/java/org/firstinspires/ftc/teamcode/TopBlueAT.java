1f7b0bde95bba178774c0ced940d7206b0ec376d
DeEric Burns <dburns26@jcss.us>
2023-11-10 10:24:37 -0500

    Added basic trajectory sequence.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAT.java
@@ -1,11 +1,23 @@
 package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
 @Autonomous(name = "TopBlue", group = "17421 Autonomous")
 public class TopBlueAT extends OpMode {
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     @Override
     public void init(){
+        TrajectorySequence traj = drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
+                .turn(Math.toRadians(90))
+                .forward(10)
+                .build();
+        drive.followTrajectorySequence(traj);
 
     }
 
