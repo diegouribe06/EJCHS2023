3d1c18d7f1a92749989240f52ffccb1d178c7349
nehemiah <nehemiahgarcia07@gmail.com>
2024-01-04 15:21:00 -0500

    Slide working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -125,7 +125,7 @@ public class Auto extends LinearOpMode {
             // choose one of the following:
             //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
             //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            .setModelAssetName("17421IR.tfile")
+            .setModelAssetName("17421IR.tflite")
             //.setModelFileName(TFOD_MODEL_FILE)
 
             // The following default settings are available to un-comment and edit as needed to 
