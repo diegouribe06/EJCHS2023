8b8ca71e7a534927359525f552a6ecdbf2565349
DeEric Burns <dburns26@jcss.us>
2024-01-25 15:49:47 -0500

    Temp HPAT code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HPBlueAT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HPBlueAT.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
@@ -10,7 +11,50 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 public class HPBlueAT extends ATCore {
     @Override
     public void runOpMode(){
+        super.runOpMode();
         TrajectorySequence middleTrajectory = drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
+                .strafeRight(strafeCorrect(4))
+                .addTemporalMarker(() -> {
+                    hookLeftServo.setPosition(0.7);
+                    hookRightServo.setPosition(0.7);
+                })
+                .waitSeconds(1)
+                .back(positionCorrect(74))
+                .strafeRight(strafeCorrect(23))
+                .turn(Math.toRadians(-8))
+                .addTemporalMarker(() -> {
+                    hookLeftServo.setPosition(0.7);
+                    hookRightServo.setPosition(0.7);
+                    bucketDoor.setPosition(0.925);
+                    extendSlide(-1375);
+                })
+                .waitSeconds(0.75)
+                .addTemporalMarker(() -> {
+                    bucketRotate.setPosition(0.85);
+                    //bucketArm.setPosition(0.5);
+                })
+                .waitSeconds(0.75)
+                .addTemporalMarker(() -> {
+                    bucketDoor.setPosition(0.75);
+                })
+                .waitSeconds(0.75)
+                .addTemporalMarker(() -> {
+                    bucketRotate.setPosition(0.45);
+                    bucketArm.setPosition(0.8479);
+                })
+                .waitSeconds(0.75)
+                .UNSTABLE_addDisplacementMarkerOffset(2, () -> {
+                    slideMotor.setTargetPosition(-100);
+                    slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    slideMotor.setPower(1);
+                })
+                .waitSeconds(3)
+                .strafeRight(strafeCorrect(21))
+                .turn(Math.toRadians(-13))
+                .back(positionCorrect(14))
+                .addTemporalMarker(() -> {
+                    bucketArm.setPosition(0);
+                })
                 .build();
         TrajectorySequence leftTrajectory = drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
                 .build();
@@ -18,7 +62,6 @@ public class HPBlueAT extends ATCore {
                 .build();
 
         waitForStart();
-        super.runOpMode();
 
         if(elementSide == "middle"){
             drive.followTrajectorySequence(middleTrajectory);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HPRedAT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HPRedAT.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
@@ -12,6 +13,39 @@ public class HPRedAT extends ATCore{
     public void runOpMode(){
         super.runOpMode();
         TrajectorySequence middleTrajectory = drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
+                .strafeLeft(strafeCorrect(4))
+                .addTemporalMarker(() -> {
+                    hookLeftServo.setPosition(0.7);
+                    hookRightServo.setPosition(0.7);
+                })
+                .waitSeconds(1)
+                .back(positionCorrect(70))
+                .strafeLeft(strafeCorrect(23))
+                .turn(Math.toRadians(8))
+                .addTemporalMarker(() -> {
+                    hookLeftServo.setPosition(0.7);
+                    hookRightServo.setPosition(0.7);
+                    bucketDoor.setPosition(0.925);
+                    extendSlide(-1375);
+                })
+                .waitSeconds(1)
+                .addTemporalMarker(() -> {
+                    bucketRotate.setPosition(0.85);
+                })
+                .waitSeconds(1)
+                .addTemporalMarker(() -> {
+                    bucketDoor.setPosition(0.75);
+                })
+                .waitSeconds(1)
+                .UNSTABLE_addDisplacementMarkerOffset(2, () -> {
+                    slideMotor.setTargetPosition(-100);
+                    slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    slideMotor.setPower(1);
+                })
+                .waitSeconds(3)
+                .strafeLeft(strafeCorrect(18))
+                .turn(Math.toRadians(13))
+                .back(positionCorrect(12))
                 .build();
         TrajectorySequence leftTrajectory = drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
                 .build();
@@ -19,7 +53,6 @@ public class HPRedAT extends ATCore{
                 .build();
 
         waitForStart();
-        super.runOpMode();
 
         if(elementSide == "middle"){
             drive.followTrajectorySequence(middleTrajectory);
